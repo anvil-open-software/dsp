75c885280f9557629739abb14c7966f79e454a28
mitchemr <michael.mitchell@dematic.com>
2017-05-22 10:38:14 -0700

    DLABS-1229, Driver configuration.

--- src/main/scala/com/dematic/labs/dsp/configuration/DriverConfiguration.scala
+++ src/main/scala/com/dematic/labs/dsp/configuration/DriverConfiguration.scala
@@ -1,4 +1,8 @@
 package com.dematic.labs.dsp.configuration
 
-class DriverConfiguration(val appName: String, val masterUrl: String) {
+import com.typesafe.config._
+
+class DriverConfiguration() {
+  private val config =  ConfigFactory.load()
+  private lazy val root = config.getConfig("driverApp")
 }
