e31099a5d2698cfc54932944c1dfe52896f00fec
mitchemr <michael.mitchell@dematic.com>
2017-05-23 09:17:53 -0700

    DLABS-1229, Driver configuration, added more properties.

--- src/main/scala/com/dematic/labs/dsp/configuration/DriverConfiguration.scala
+++ src/main/scala/com/dematic/labs/dsp/configuration/DriverConfiguration.scala
@@ -2,7 +2,19 @@ package com.dematic.labs.dsp.configuration
 
 import com.typesafe.config._
 
-class DriverConfiguration() {
+class DriverConfiguration {
   private val config =  ConfigFactory.load()
-  private lazy val root = config.getConfig("driverApp")
+
+  object driver {
+     lazy val appName: String = config.getString("driver.appName")
+     lazy val masterUrl: String = config.getString("driver.masterUrl")
+  }
+
+  object spark {
+    lazy val checkpointDir: String = config.getString("spark.checkpoint.dir")
+    lazy val sqlShufflePartition: String = config.getString("sql.shuffle.partition")
+    lazy val outputMode: String = config.getString("output.mode")
+    lazy val watermarkTime: String = config.getString("watermark.time")
+    lazy val queryTrigger: String = config.getString("query.trigger")
+  }
 }
