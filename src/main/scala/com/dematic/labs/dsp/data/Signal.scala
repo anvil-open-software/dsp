6321bde5d56d7122f84ab1f81662832502bccde2
mitchemr <michael.mitchell@dematic.com>
2017-06-05 13:51:05 -0700

    DLABS-1261, Throughput simulator.

--- src/main/scala/com/dematic/labs/dsp/data/Signal.scala
+++ src/main/scala/com/dematic/labs/dsp/data/Signal.scala
@@ -3,18 +3,20 @@ package com.dematic.labs.dsp.data
 import scala.beans.BeanProperty
 import scala.util.hashing.MurmurHash3
 
-class Signal(@BeanProperty var id: String, @BeanProperty var value: String, @BeanProperty var timestamp: String) {
+class Signal(@BeanProperty var id: Long, @BeanProperty var value: Int, @BeanProperty var timestamp: String,
+             @BeanProperty val generatorId: String) {
   override def equals(other: Any): Boolean = other match {
     case that: Signal => (that canEqual this) &&
       that.id == this.id &&
       that.value == this.value &&
       that.timestamp == this.timestamp
+      that.generatorId == this.generatorId
     case _ => false
   }
 
   def canEqual(other: Any): Boolean = other.isInstanceOf[Signal]
 
-  override def hashCode(): Int = MurmurHash3.seqHash(List(id, value, timestamp))
+  override def hashCode(): Int = MurmurHash3.seqHash(List(id, value, timestamp, generatorId))
 
-  override def toString: String = String format("%s, %s, %s", id, value, timestamp)
+  override def toString = s"Signal($id, $value, $timestamp, $generatorId)"
 }
--- src/main/scala/com/dematic/labs/dsp/simulators/Throughput.scala
+++ src/main/scala/com/dematic/labs/dsp/simulators/Throughput.scala
@@ -9,30 +9,43 @@ import com.dematic.labs.dsp.producers.kafka.Producer
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.util.Random
 
 object Throughput extends App {
+  // define how long to run the throughput simulator
+  CountdownTimer.countDown(args(0).toInt)
+  // configuration
   val config = new DriverConfiguration
+  // overridden value specific to producer
+  val generatorId = s"${config.Driver.appName}${args(1)}"
+
   val producer = Producer(config)
-  CountdownTimer.countDown(1)
+  // generated values
+  private val nextId = {
+    var id: Long = 1
+    () => {
+      id += 1
+      id
+    }
+  }
+  private val nextRandomValue = {
+    val random = new Random
+    () => {
+      random.nextInt()
+    }
+  }
+
   // fire and forget, until timer is finished
   while (!CountdownTimer.isFinished) {
     val result = Future {
       // create random json
-      val json = toJson(new Signal(generateRandomId(), generateRandomValue(), Instant.now.toString))
+      val json = toJson(new Signal(nextId(), nextRandomValue(), Instant.now.toString, generatorId))
       producer.send(json)
     }
     // only print exception if, something goes wrong
     result onFailure {
-      case t => println(s"Unexpected Error: ${t.getMessage}")
+      case t => println(s"Unexpected Error: ${t.printStackTrace()}")
     }
   }
   producer.close()
-
-  private def generateRandomId(): String = {
-    ""
-  }
-
-  private def generateRandomValue(): String = {
-    ""
-  }
 }
