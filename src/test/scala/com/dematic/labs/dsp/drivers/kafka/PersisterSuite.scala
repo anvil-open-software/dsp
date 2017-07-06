81e9fb0f1f01d608e52d6a8b4bbb84ba593a78f0
mitchemr <michael.mitchell@dematic.com>
2017-07-06 09:24:54 -0700

    DLABS-1310, Create cassandra table.

--- src/test/scala/com/dematic/labs/dsp/drivers/kafka/PersisterSuite.scala
+++ src/test/scala/com/dematic/labs/dsp/drivers/kafka/PersisterSuite.scala
@@ -1,10 +1,13 @@
 package com.dematic.labs.dsp.drivers.kafka
 
+import java.io.InputStream
 
 import com.datastax.driver.core.Cluster
 import info.batey.kafka.unit.KafkaUnit
 import org.cassandraunit.utils.EmbeddedCassandraServerHelper
 import org.scalatest.{BeforeAndAfter, FunSuite}
+
+import scala.io.Source
 
 class PersisterSuite extends FunSuite with BeforeAndAfter {
 
@@ -24,22 +27,35 @@ class PersisterSuite extends FunSuite with BeforeAndAfter {
     kafkaServer.shutdown()
   }
 
-
   test("complete DSP Persister test, push signals to kafka, spark consumes and persist to cassandra") {
     // create a cassandra cluster and connect and create keyspace and table
-    val cluster: Cluster =
-      Cluster.builder().addContactPoint(EmbeddedCassandraServerHelper.getHost).withPort(EmbeddedCassandraServerHelper.getNativeTransportPort).build
-
-    try {
-      val session = cluster.connect
-      session.execute(s"CREATE KEYSPACE if not exists $topicAndKeyspace WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };")
-      // ensure keyspace was created
-      assert(cluster.getMetadata.getKeyspace(topicAndKeyspace).getName === topicAndKeyspace)
-      // create cassandra table
-
-
-    } finally {
-      cluster.close()
+    using(Cluster.builder().
+      addContactPoint(EmbeddedCassandraServerHelper.getHost).
+      withPort(EmbeddedCassandraServerHelper.getNativeTransportPort).build) {
+      cluster => {
+        using(cluster.connect) {
+          session => {
+            session.execute(s"CREATE KEYSPACE if not exists $topicAndKeyspace WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };")
+            // ensure keyspace was created
+            assert(cluster.getMetadata.getKeyspace(topicAndKeyspace).getName === topicAndKeyspace)
+            // move to keyspace
+            session.execute(s"USE $topicAndKeyspace;")
+            // create table from cql
+            val stream: InputStream = getClass.getResourceAsStream("/persister.cql")
+            for (line <- Source.fromInputStream(stream).getLines) {
+              if (!line.startsWith("#")) session.execute(line)
+            }
+          }
+        }
+      }
     }
   }
+
+  // Automatically closing the resource
+  def using[A <: {def close() : Unit}, B](resource: A)(f: A => B): B =
+    try {
+      f(resource)
+    } finally {
+      resource.close()
+    }
 }
