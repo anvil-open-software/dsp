39ad821e2a08ed518fe723afb8b9595e9f03c724
mitchemr <michael.mitchell@dematic.com>
2017-08-29 15:25:48 -0700

    DLABS-1394, signal aggregation continued.

--- src/main/java/com/dematic/labs/dsp/drivers/SignalAggregation.java
+++ src/main/java/com/dematic/labs/dsp/drivers/SignalAggregation.java
@@ -1,10 +1,14 @@
 package com.dematic.labs.dsp.drivers;
 
+import com.datastax.driver.core.Statement;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
 import com.datastax.spark.connector.cql.CassandraConnector;
 import com.dematic.labs.analytics.monitor.spark.MonitorConsts;
 import com.dematic.labs.analytics.monitor.spark.PrometheusStreamingQueryListener;
 import com.google.common.base.Strings;
 import org.apache.spark.sql.*;
+import org.apache.spark.sql.streaming.StreamingQuery;
+import org.apache.spark.sql.streaming.StreamingQueryException;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 import scala.Symbol;
@@ -15,8 +19,8 @@ import static org.apache.spark.sql.types.DataTypes.*;
 /**
  * -Dconfig.file=path/to/file/signalAggregation.conf
  */
-public class SignalAggregation {
-    public static void main(final String[] args) {
+public final class SignalAggregation {
+    public static void main(final String[] args) throws StreamingQueryException {
         // create the spark session
         final SparkSession.Builder builder = SparkSession.builder();
         final String masterUrl = Spark$.MODULE$.masterUrl();
@@ -59,7 +63,7 @@ public class SignalAggregation {
 
             // convert to json and select all values
             final Dataset<Row> signals = kafka.selectExpr("cast (value as string) as json").
-                    select(functions.from_json(new Column("json"), schema)).as("signals").select("signals.*");
+                    select(functions.from_json(new Column("json"), schema)).as("signals").select( "signals.*");
 
             // todo: looking watermark time
             final Dataset<Row> aggregate = signals.groupBy(
@@ -72,7 +76,36 @@ public class SignalAggregation {
                             functions.sum("value"));
 
             // write to cassandra
+            final StreamingQuery query = aggregate.writeStream()
+                    .option(Spark$.MODULE$.CheckpointLocationKey(), Spark$.MODULE$.checkpointLocation())
+                    .queryName("signalAggregation")
+                    .foreach(new ForeachWriter<Row>() {
+                        @Override
+                        public void process(final Row value) {
+                            cassandraConnector.withSessionDo(session -> session.execute(cql(value)));
+                        }
 
+                        @Override
+                        public void close(final Throwable errorOrNull) {
+                        }
+
+                        @Override
+                        public boolean open(final long partitionId, final long version) {
+                            return true;
+                        }
+
+                        private Statement cql(final Row row) {
+                            return QueryBuilder.update(Cassandra$.MODULE$.keyspace(), "signal_aggregation")
+                                    .with(QueryBuilder.set("count", row.getAs(2)))
+                                    .and(QueryBuilder.set("avg", row.getAs(3)))
+                                    .and(QueryBuilder.set("min", row.getAs(4)))
+                                    .and(QueryBuilder.set("max", row.getAs(5)))
+                                    .and(QueryBuilder.set("sum", row.getAs(6)))
+                                    .where(QueryBuilder.eq("opc_tag_id", row.getAs(1)))
+                                    .and(QueryBuilder.eq("aggregate", row.get(0)));
+                        }
+                    }).start();
+            query.awaitTermination();
         } finally {
             sparkSession.close();
         }
