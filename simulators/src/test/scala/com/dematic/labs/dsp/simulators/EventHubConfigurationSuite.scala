b3266a67e5c834d2d18a023a9fcab1a3230539a5
mitchemr <michael.mitchell@dematic.com>
2017-10-24 15:35:32 -0700

    DLABS-1536, Added unit test.

---
+++ simulators/src/test/scala/com/dematic/labs/dsp/simulators/EventHubConfigurationSuite.scala
@@ -0,0 +1,22 @@
+package com.dematic.labs.dsp.simulators
+
+import org.scalatest.FunSuite
+
+class EventHubConfigurationSuite extends FunSuite {
+  // set system property to ensure correct file used.
+  System.setProperty("config.resource", "rateLimitedEventHubProducer.conf")
+
+  test("event hub configuration from conf file") {
+    val config = new EventHubConfiguration.Builder().build
+    // producer config
+    assert(config.getId === "eventhub")
+    assert(config.getSignalIdRangeLow === 100)
+    assert(config.getSignalIdRangeHigh === 200)
+    assert(config.getDurationInMinutes === 3)
+    // eventhub config
+    assert(config.getNamespace === "myNamespace")
+    assert(config.getName === "myName")
+    assert(config.getAccessKeyName === "mySharedAccessKeyName")
+    assert(config.getAccessKey === "mySharedAccessKey")
+  }
+}
