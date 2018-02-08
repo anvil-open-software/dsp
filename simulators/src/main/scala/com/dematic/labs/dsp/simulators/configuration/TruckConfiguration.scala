7e2a4bcc410b02706b72af056fc016c9dc22087f
mitchemr <michael.mitchell@dematic.com>
2018-02-08 11:01:31 -0800

    DLABS-1736, More Truck configuration.

--- simulators/src/main/scala/com/dematic/labs/dsp/simulators/configuration/TruckConfiguration.scala
+++ simulators/src/main/scala/com/dematic/labs/dsp/simulators/configuration/TruckConfiguration.scala
@@ -1,14 +1,20 @@
 package com.dematic.labs.dsp.simulators.configuration
 
+import java.util
+
 import com.dematic.labs.toolkit_bigdata.simulators.configuration.ProducerConfiguration
+import com.google.common.collect.Iterables
 
 object TruckConfiguration {
-  class Builder() extends ProducerConfiguration.Builder[TruckConfiguration.Builder] {
-    private val URL = "influxdb.url"
-    private val DATABASE = "influxdb.database"
-    private val USERNAME = "influxdb.username"
-    private val PASSWORD = "influxdb.password"
 
+  class Builder() extends ProducerConfiguration.Builder[TruckConfiguration.Builder] {
+    private var TRUCK_ID_RANGE = "producer.truckIdRange"
+    private val URL = "producer.influxdb.url"
+    private val DATABASE = "producer.influxdb.database"
+    private val USERNAME = "producer.influxdb.username"
+    private val PASSWORD = "producer.influxdb.password"
+
+    val truckIdRange: util.List[Integer] = getConfig.getIntList(TRUCK_ID_RANGE)
     val url: String = getConfig.getString(URL)
     val database: String = getConfig.getString(DATABASE)
     val username: String = getConfig.getString(USERNAME)
@@ -22,15 +28,28 @@ object TruckConfiguration {
       new TruckConfiguration(this)
     }
   }
+
 }
 
 final class TruckConfiguration private[TruckConfiguration](val builder: TruckConfiguration.Builder)
   extends ProducerConfiguration(builder) {
-
+  private val truckIdRange = builder.truckIdRange
   private val url = builder.url
   private val database = builder.database
   private val username = builder.username
   private val password = builder.password
+
+  def getTruckIdRange: util.List[Integer] = {
+    truckIdRange
+  }
+
+  def getTruckIdRangeLow: Integer = {
+    Iterables.getFirst(truckIdRange, new Integer(0))
+  }
+
+  def getTruckIdRangeHigh: Integer = {
+    Iterables.getLast(truckIdRange, new Integer(0))
+  }
 
   def getUrl: String = {
     url
