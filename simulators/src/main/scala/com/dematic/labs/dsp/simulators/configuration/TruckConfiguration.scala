984e2ca26b96036f3cea6a716231c38d7e45b0fc
mitchemr <michael.mitchell@dematic.com>
2018-02-08 10:21:59 -0800

    DLABS-1736, Truck configuration.

--- simulators/src/main/scala/com/dematic/labs/dsp/simulators/configuration/TruckConfiguration.scala
+++ simulators/src/main/scala/com/dematic/labs/dsp/simulators/configuration/TruckConfiguration.scala
@@ -4,10 +4,12 @@ import com.dematic.labs.toolkit_bigdata.simulators.configuration.ProducerConfigu
 
 object TruckConfiguration {
   class Builder() extends ProducerConfiguration.Builder[TruckConfiguration.Builder] {
+    private val URL = "influxdb.url"
     private val DATABASE = "influxdb.database"
     private val USERNAME = "influxdb.username"
     private val PASSWORD = "influxdb.password"
 
+    val url: String = getConfig.getString(URL)
     val database: String = getConfig.getString(DATABASE)
     val username: String = getConfig.getString(USERNAME)
     val password: String = getConfig.getString(PASSWORD)
@@ -25,9 +27,14 @@ object TruckConfiguration {
 final class TruckConfiguration private[TruckConfiguration](val builder: TruckConfiguration.Builder)
   extends ProducerConfiguration(builder) {
 
+  private val url = builder.url
   private val database = builder.database
   private val username = builder.username
   private val password = builder.password
+
+  def getUrl: String = {
+    url
+  }
 
   def getDatabase: String = {
     database
