7054dc974516bd967ba8e22fed9e86b4e15aa478
mitchemr <michael.mitchell@dematic.com>
2018-02-22 09:57:01 -0800

    DLABS-1797, Initial truck alert driver.

---
+++ drivers/src/main/scala/com/dematic/labs/dsp/drivers/TruckAlerts.scala
@@ -0,0 +1,77 @@
+package com.dematic.labs.dsp.drivers
+
+import com.dematic.labs.analytics.monitor.spark.{MonitorConsts, PrometheusStreamingQueryListener}
+import com.dematic.labs.dsp.drivers.configuration.{DefaultDriverConfiguration, DriverConfiguration}
+import com.google.common.base.Strings
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.from_json
+import org.apache.spark.sql.streaming.Trigger.ProcessingTime
+import org.apache.spark.sql.types._
+
+object TruckAlerts {
+  // should only be  used with testing
+  private var injectedDriverConfiguration: DriverConfiguration = _
+
+  private[drivers] def setDriverConfiguration(driverConfiguration: DriverConfiguration) {
+    injectedDriverConfiguration = driverConfiguration
+  }
+
+  def main(args: Array[String]) {
+    // driver configuration
+    val config = if (injectedDriverConfiguration == null) {
+      new DefaultDriverConfiguration.Builder().build
+    } else {
+      injectedDriverConfiguration
+    }
+
+    // create the spark session
+    val builder: SparkSession.Builder = SparkSession.builder
+    if (!Strings.isNullOrEmpty(config.getSparkMaster)) builder.master(config.getSparkMaster)
+    builder.appName(config.getDriverAppName)
+    val sparkSession: SparkSession = builder.getOrCreate
+
+    // hook up Prometheus listener for monitoring
+    if (sys.props.contains(MonitorConsts.SPARK_QUERY_MONITOR_PUSH_GATEWAY)) {
+      sparkSession.streams.addListener(new PrometheusStreamingQueryListener(sparkSession.sparkContext.getConf,
+        config.getDriverAppName))
+    }
+
+    // create the kafka input source
+    try {
+      val kafka = sparkSession.readStream
+        .format("kafka")
+        .option("kafka.bootstrap.servers", config.getKafkaBootstrapServers)
+        .option(config.getKafkaSubscribe, config.getKafkaTopics)
+        .option("startingOffsets", config.getKafkaStartingOffsets)
+        .load
+
+      // define the truck json schema
+      val schema: StructType = StructType(Seq(
+        StructField("truck", StringType, nullable = false),
+        StructField("_timestamp", StringType, nullable = false),
+        StructField("channel", StringType, nullable = false),
+        StructField("value", LongType, nullable = false),
+        StructField("unit", StringType, nullable = false)
+      ))
+
+      import sparkSession.implicits._
+
+      // convert to json and select all channels
+      val channels = kafka.selectExpr("cast (value as string) as json").
+        select(from_json($"json", schema).as("channels")).select("channels.*")
+
+      //2) just write to the console
+      channels.writeStream
+        .format("memory")
+        .trigger(ProcessingTime(config.getSparkQueryTrigger))
+        .option("spark.sql.streaming.checkpointLocation", config.getSparkCheckpointLocation)
+        .queryName("truckAlerts")
+        .outputMode(config.getSparkOutputMode)
+        .start
+
+      // keep alive
+      sparkSession.streams.awaitAnyTermination
+    } finally
+      sparkSession.close
+  }
+}
