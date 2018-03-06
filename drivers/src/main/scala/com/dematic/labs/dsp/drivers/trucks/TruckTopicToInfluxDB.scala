15b6ac3b032f8003f011847c89ad960e1aa15bcf
kimh <heji.kim.ext@dematic.com>
2018-03-05 18:44:42 -0800

    test influxdb writer

---
+++ drivers/src/main/scala/com/dematic/labs/dsp/drivers/trucks/TruckTopicToInfluxDB.scala
@@ -0,0 +1,103 @@
+package com.dematic.labs.dsp.drivers.trucks
+
+import java.util.concurrent.TimeUnit
+
+import com.dematic.labs.analytics.monitor.spark.{MonitorConsts, PrometheusStreamingQueryListener}
+import com.dematic.labs.dsp.drivers.configuration.{DefaultDriverConfiguration, DriverConfiguration}
+import com.google.common.base.Strings
+import com.typesafe.config.ConfigFactory
+import okhttp3.OkHttpClient
+import org.apache.spark.sql.{ForeachWriter, Row, SparkSession}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.streaming.Trigger._
+import org.influxdb.{InfluxDB, InfluxDBFactory}
+
+object TruckTopicToInfluxDB {
+  // should only be  used with testing
+  private var injectedDriverConfiguration: DriverConfiguration = _
+
+  private[drivers] def setDriverConfiguration(driverConfiguration: DriverConfiguration) {
+    injectedDriverConfiguration = driverConfiguration
+  }
+
+  def main(args: Array[String]) {
+    // driver configuration
+    val config = if (injectedDriverConfiguration == null) {
+      new DefaultDriverConfiguration.Builder().build
+    } else {
+      injectedDriverConfiguration
+    }
+
+    // create the spark session
+    val builder: SparkSession.Builder = SparkSession.builder
+    if (!Strings.isNullOrEmpty(config.getSparkMaster)) builder.master(config.getSparkMaster)
+    builder.appName(config.getDriverAppName)
+    val sparkSession: SparkSession = builder.getOrCreate
+
+    // hook up Prometheus listener for monitoring
+    if (sys.props.contains(MonitorConsts.SPARK_QUERY_MONITOR_PUSH_GATEWAY)) {
+      sparkSession.streams.addListener(new PrometheusStreamingQueryListener(sparkSession.sparkContext.getConf,
+        config.getDriverAppName))
+    }
+
+
+
+    // create the connection to influxDb with more generous timeout instead of default 10 seconds
+    val httpClientBuilder = new OkHttpClient.Builder().readTimeout(120, TimeUnit.SECONDS)
+      .connectTimeout(120, TimeUnit.SECONDS)
+
+    // todo hook in parms if it works
+    private val influxDB: InfluxDB = InfluxDBFactory.connect("http://10.207.208.10:8086", "kafka", "kafka", httpClientBuilder)
+    influxDB.setDatabase("ccd_test")
+
+    // create the kafka input source
+    try {
+      val kafka = sparkSession.readStream
+        .format("kafka")
+        .option("kafka.bootstrap.servers", config.getKafkaBootstrapServers)
+        .option(config.getKafkaSubscribe, config.getKafkaTopics)
+        .option("startingOffsets", config.getKafkaStartingOffsets)
+        .option("maxOffsetsPerTrigger",config.getKafkaMaxOffsetsPerTrigger)
+        .load
+
+      // define the truck json schema
+      val schema: StructType = StructType(Seq(
+        StructField("truck", StringType, nullable = false),
+        StructField("_timestamp", TimestampType, nullable = false),
+        StructField("channel", StringType, nullable = false),
+        StructField("value", DoubleType, nullable = false),
+        StructField("unit", StringType, nullable = false)
+      ))
+
+      // convert to json and select only channel 'T_motTemp_Lft'
+      val channels = kafka.selectExpr("cast (value as string) as json").
+        select(from_json($"json", schema) as "channels").
+        select("channels.*").
+        where("channel == 'T_motTemp_Lft'")
+      val formatter = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'.'SSSX")
+      val persister = channels.writeStream
+        .trigger(ProcessingTime(config.getSparkQueryTrigger))
+        .option("spark.sql.streaming.checkpointLocation", config.getSparkCheckpointLocation)
+        .queryName("truckAlerts")
+        .foreach(new ForeachWriter[Row]() {
+          override def open(partitionId: Long, version: Long) = true
+
+          override def process(row: Row) {
+            val batchRequestBody: scala.collection.mutable.StringBuilder = new scala.collection.mutable.StringBuilder()
+
+            val timestamp= row.getAs[String]("_timestamp");
+            val timeInNanoseconds = " " + (formatter.parse(timestamp).getTime * 1000000);
+            val line = row.getAs[String]("channel") + ",truck=" + row.getAs[String]("truck") + " value=" + row.getAs[Double]("value") + timeInNanoseconds
+            batchRequestBody.append(line + "\n")
+            influxDB.write(batchRequestBody.toString());
+          }
+          override def close(errorOrNull: Throwable) {}
+          }).start
+
+      // keep alive
+      sparkSession.streams.awaitAnyTermination
+    } finally
+      sparkSession.close
+  }
+}
