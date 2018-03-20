bae91d3a085ccbe3c9ff88d4a58ae8f181560f51
mitchemr <michael.mitchell@dematic.com>
2018-03-20 16:06:59 -0700

    DLABS-1848 - Temp UDAF

--- drivers/src/main/scala/com/dematic/labs/dsp/drivers/TruckAlerts.scala
+++ drivers/src/main/scala/com/dematic/labs/dsp/drivers/TruckAlerts.scala
@@ -3,10 +3,11 @@ package com.dematic.labs.dsp.drivers
 import com.dematic.labs.analytics.monitor.spark.{MonitorConsts, PrometheusStreamingQueryListener}
 import com.dematic.labs.dsp.drivers.configuration.{DefaultDriverConfiguration, DriverConfiguration}
 import com.google.common.base.Strings
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.Trigger.ProcessingTime
 import org.apache.spark.sql.types._
+import org.apache.spark.sql.{ForeachWriter, Row, SparkSession}
 
 object TruckAlerts {
   // should only be  used with testing
@@ -36,6 +37,7 @@ object TruckAlerts {
         config.getDriverAppName))
     }
 
+    var increasedTemperature = new IncreasedTemperature(10)
     // create the kafka input source
     try {
       val kafka = sparkSession.readStream
@@ -66,21 +68,198 @@ object TruckAlerts {
       val alerts = channels.
         withWatermark("_timestamp", "1 minutes"). // only keep old data for 1 minutes for late updates
         groupBy(window('_timestamp, "60 minutes") as "alert_time", 'truck).
-        agg(max('value) as "max", min('value) as "min", collect_list(struct('_timestamp, 'value)) as "values").
-        where('max - 'min > 10)
+        agg(increasedTemperature('_timestamp, 'value))
+
+
+      alerts.printSchema()
 
       // just write to the console
       alerts.writeStream
-        .format("console")
         .trigger(ProcessingTime(config.getSparkQueryTrigger))
         .option("spark.sql.streaming.checkpointLocation", config.getSparkCheckpointLocation)
         .queryName("truckAlerts")
         .outputMode("update")
+
+        .foreach(new ForeachWriter[Row]() {
+          override def open(partitionId: Long, version: Long) = true
+
+          override def process(row: Row) {
+            println(row.toString())
+          }
+
+          override def close(errorOrNull: Throwable) {}
+
+        })
         .start
 
       // keep alive
       sparkSession.streams.awaitAnyTermination
     } finally
       sparkSession.close
+  }
+}
+
+class IncreasedTemperature(threshold: Int) extends UserDefinedAggregateFunction {
+  // This is the input fields for your aggregate function.
+  override def inputSchema: org.apache.spark.sql.types.StructType =
+    StructType(
+      Array(
+        StructField("_timestamp", TimestampType),
+        StructField("value", DoubleType)
+      )
+    )
+
+  // This is the internal fields you keep for computing your aggregate.
+  override def bufferSchema: StructType = StructType(
+    Array(
+      StructField("min", DoubleType),
+      StructField("alerts", ArrayType(
+        StructType(
+          Array(
+            StructField("timestamp", TimestampType),
+            StructField("value", DoubleType))
+        ))
+      )
+    )
+  )
+
+  // This is the output type of your aggregatation function.
+  override def dataType: DataType = StructType(
+    Array(
+      StructField("alerts", ArrayType(
+        StructType(
+          Array(
+            StructField("timestamp", TimestampType),
+            StructField("value", DoubleType))
+        ))
+      )
+    )
+  )
+
+  override def deterministic: Boolean = true
+
+  // This is the initial value for your buffer schema.
+  override def initialize(buffer: MutableAggregationBuffer): Unit = {
+    //   buffer(0) = null
+  }
+
+  // This is how to update your buffer schema given an input.
+  override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
+    /*  // keep the minimum
+      buffer(0) =
+        if (buffer.isNullAt(0)) input.getAs[Double](0)
+        else if (input.getAs[Double](0) > buffer.getAs[Double](0)) buffer.getAs[Double](0)
+        else input.getAs[Double](0)*/
+  }
+
+  // This is how to merge two objects with the bufferSchema type.
+  override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
+    /* if (buffer1(0) == null) {
+       buffer1(0) = buffer2(0)
+     }
+
+     if (buffer1(0) != null) {
+       buffer1(0) = if (buffer1.getAs[Double](0) > buffer2.getAs[Double](0)) buffer2.getAs[Double](0) else buffer1.getAs[Double](0)
+
+     }*/
+  }
+
+  // This is where you output the final value, given the final value of your bufferSchema.
+  override def evaluate(buffer: Row): Any = {
+    buffer(0)
+  }
+}
+
+
+// [[2017-01-17 03:00:00.0,2017-01-17 04:00:00.0],
+//      H2X3100,
+//        WrappedArray([2017-01-17 03:35:30.079,37.0])]
+
+
+// [[2017-01-17 03:00:00.0,2017-01-17 04:00:00.0],
+//      H2X3100,
+//        WrappedArray([2017-01-17 03:35:32.079,37.0], [2017-01-17 03:35:31.079,37.0], [2017-01-17 03:35:33.079,37.0], [2017-01-17 03:35:30.079,37.0])]
+
+
+private class IncreasedTempThreshold extends UserDefinedAggregateFunction {
+
+  /*override def inputSchema: org.apache.spark.sql.types.StructType =
+    StructType(Seq(StructField("value", DoubleType), StructField("value2", DoubleType)))*/
+
+
+  /*
+
+
+  values: array (nullable = true)
+ |    |-- element: struct (containsNull = true)
+ |    |    |-- _timestamp: timestamp (nullable = true)
+ |    |    |-- value: double (nullable = true)
+
+
+
+
+root
+ |-- alert_time: struct (nullable = true)
+ |    |-- start: timestamp (nullable = true)
+ |    |-- end: timestamp (nullable = true)
+ |-- truck: string (nullable = true)
+
+
+ |-- values: array (nullable = true)
+ |    |-- element: struct (containsNull = true)
+ |    |    |-- _timestamp: timestamp (nullable = true)
+ |    |    |-- value: double (nullable = true)
+
+
+
+val schema = StructType(
+      Array(
+        StructField("id", StringType),
+        StructField("name", StringType),
+        StructField("score", ArrayType(StructType(Array(
+          StructField("keyword", StringType),
+          StructField("point", IntegerType)
+        ))))
+      )
+    )
+
+
+    */
+  override def inputSchema: StructType = StructType(
+    Array(
+      StructField("values", ArrayType(
+        StructType(
+          Array(
+            StructField("_timestamp", TimestampType),
+            StructField("value", DoubleType)
+          )
+        )
+      )))
+  )
+
+
+  override def bufferSchema: StructType = StructType(
+    StructField("product", DoubleType) :: Nil
+  )
+
+  override def dataType: DataType = DoubleType
+
+  override def deterministic: Boolean = true
+
+  override def initialize(buffer: MutableAggregationBuffer): Unit = {
+    buffer(0) = 0.0
+  }
+
+  override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
+    buffer(0) = buffer.getAs[Double](0) + input.getAs[Double](0) * input.getAs[Double](0)
+  }
+
+  override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
+    buffer1(0) = buffer1.getAs[Double](0) + buffer2.getAs[Double](0)
+
+  }
+
+  override def evaluate(buffer: Row): Any = {
+    buffer.getDouble(0)
   }
 }
