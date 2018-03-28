14e323a150b1c0e7042e2b4acb2054bfc8348f62
mitchemr <michael.mitchell@dematic.com>
2018-03-28 12:18:16 -0700

    DLABS-1848 - Changed the UDAF to return a count.

--- drivers/src/main/scala/com/dematic/labs/dsp/drivers/TruckAlerts.scala
+++ drivers/src/main/scala/com/dematic/labs/dsp/drivers/TruckAlerts.scala
@@ -2,12 +2,15 @@ package com.dematic.labs.dsp.drivers
 
 import com.dematic.labs.analytics.monitor.spark.{MonitorConsts, PrometheusStreamingQueryListener}
 import com.dematic.labs.dsp.drivers.configuration.{DefaultDriverConfiguration, DriverConfiguration}
-import com.dematic.labs.dsp.drivers.functions.IncreasingTemperatureAlert
 import com.google.common.base.Strings
+import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.Trigger.ProcessingTime
+import org.apache.spark.sql.types.DataTypes.StringType
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.{ForeachWriter, Row, SparkSession}
+
+import scala.collection.mutable.ListBuffer
 
 object TruckAlerts {
   // should only be  used with testing
@@ -37,7 +40,8 @@ object TruckAlerts {
         config.getDriverAppName))
     }
 
-    val increasingTemperatureAlert = new IncreasingTemperatureAlert(10) // todo: make configurable
+    val alertCount = new AlertCount
+
     // create the kafka input source
     try {
       val kafka = sparkSession.readStream
@@ -68,7 +72,7 @@ object TruckAlerts {
       val alerts = channels.
         withWatermark("_timestamp", "1 minutes"). // only keep old data for 1 minutes for late updates
         groupBy(window('_timestamp, "60 minutes") as "alert_time", 'truck).
-        agg(increasingTemperatureAlert('_timestamp, 'value))
+        agg(alertCount('value) as "alerts", collect_list(struct('_timestamp, 'value)) as "values").where('alerts > 0)
 
       // just write to the console
       alerts.writeStream
@@ -93,5 +97,64 @@ object TruckAlerts {
       sparkSession.streams.awaitAnyTermination
     } finally
       sparkSession.close
+  }
+
+  class AlertCount extends UserDefinedAggregateFunction {
+    // This is the input fields for your aggregate function.
+    override def inputSchema: StructType = StructType(
+      Array(
+        StructField("value", DoubleType)
+      )
+    )
+
+    // This is the internal fields you keep for computing your aggregate.
+    override def bufferSchema = StructType(
+      Array(
+        StructField("values", ArrayType(DoubleType))
+      )
+    )
+
+    // define the return type
+    override def dataType: DataType = IntegerType
+
+    // Does the function return the same value for the same input?
+    override def deterministic: Boolean = true
+
+    // Initial values
+    override def initialize(buffer: MutableAggregationBuffer): Unit = {
+      buffer.update(0, Array.empty[Double])
+    }
+
+    // Updated based on Input
+    override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
+      if (!input.isNullAt(0)) {
+        var tempArray = new ListBuffer[Double]()
+        tempArray ++= buffer.getAs[List[Double]](0)
+        val inputValues = input.getAs[Double](0)
+        tempArray += inputValues
+        buffer.update(0, tempArray)
+      }
+    }
+
+    // Merge two schemas
+    override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
+      var tempArray = new ListBuffer[Double]()
+      tempArray ++= buffer1.getAs[List[Double]](0)
+      tempArray ++= buffer2.getAs[List[Double]](0)
+      buffer1.update(0, tempArray)
+    }
+
+    // Output
+    override def evaluate(buffer: Row): Any = {
+      var count = 0
+      val mean = 17.5 //todo try to pass in
+      val threshold = 10 // std dev, needs to be configure
+
+      import scala.collection.JavaConversions._
+      for (value <- buffer.getList[Double](0)) {
+        if (math.abs(mean - value) >= threshold) count = count + 1
+      }
+      count
+    }
   }
 }
