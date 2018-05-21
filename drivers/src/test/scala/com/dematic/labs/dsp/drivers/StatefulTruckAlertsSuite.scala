3a6185a808cd4af56517515279c55c0c3337dc3f
mitchemr <michael.mitchell@dematic.com>
2018-05-21 13:54:26 -0700

    DLABS-2104, Test cleanup.

--- drivers/src/test/scala/com/dematic/labs/dsp/drivers/StatefulTruckAlertsSuite.scala
+++ drivers/src/test/scala/com/dematic/labs/dsp/drivers/StatefulTruckAlertsSuite.scala
@@ -2,7 +2,7 @@ package com.dematic.labs.dsp.drivers
 
 import java.lang
 import java.nio.file.Paths
-import java.util.concurrent.{Callable, Executors, TimeUnit}
+import java.util.concurrent.{Callable, TimeUnit}
 import java.util.{Collections, Properties}
 
 import com.dematic.labs.dsp.drivers.configuration.DriverUnitTestConfiguration
@@ -18,8 +18,9 @@ import org.scalatest.{BeforeAndAfter, FunSuite}
 import org.slf4j.{Logger, LoggerFactory}
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.{Future => ConcurrentTask}
 import scala.language.reflectiveCalls
+import scala.util.Failure
 
 class StatefulTruckAlertsSuite extends FunSuite with BeforeAndAfter {
   val logger: Logger = LoggerFactory.getLogger("StatefulTruckAlertsSuite")
@@ -27,17 +28,19 @@ class StatefulTruckAlertsSuite extends FunSuite with BeforeAndAfter {
   @Rule var checkpoint = new TemporaryFolder
 
   private val kafkaServer = new KafkaUnit
-  private val es = Executors.newCachedThreadPool
+  private var task: ConcurrentTask[Unit] = _
 
   before {
-    // create the checkpoint dir
-    checkpoint.create()
     // 1) start kafka server and create topic, only one broker created during testing
+
     kafkaServer.setKafkaBrokerConfig("offsets.topic.replication.factor", "1")
     kafkaServer.startup()
     kafkaServer.createTopic("linde", 1)
     kafkaServer.createTopic("alerts", 1)
+
     // 2) configure the driver
+
+    checkpoint.create() // create the checkpoint driver
     val uri = getClass.getResource("/truckAlerts.conf").toURI
     val config = new DriverUnitTestConfiguration.Builder(Paths.get(uri).toFile)
       .sparkCheckpointLocation(checkpoint.getRoot.getPath + "/truckAlerts")
@@ -45,27 +48,22 @@ class StatefulTruckAlertsSuite extends FunSuite with BeforeAndAfter {
       .build
     // set the configuration
     StatefulTruckAlerts.setDriverConfiguration(config)
+    // start the driver
+    task = ConcurrentTask {
+      StatefulTruckAlerts.main(Array[String]())
+    }
   }
 
   test("complete DSP StatefulTruckAlert test, send T_motTemp_Lft json messages to kafka, Spark consumes " +
     "messages and creates alerts based on the temperature value rising more then 10 degrees, then send alerts to " +
     "another kafka topic.") {
 
-    // 1) start the driver asynchronously
-    {
-      es.submit(new Runnable {
-        override def run() {
-          StatefulTruckAlerts.main(Array[String]())
-        }
-      })
-    }
-
     // timestamp used for testing
     val timestamp = DateTime.now()
 
-    // 2) push truck messages to kafka
+    // 1) push truck messages to kafka
     {
-      Future {
+      ConcurrentTask {
         // create a list of json messages to send
         val jsonMessages: List[String] = List(
           s"""{"truck":"H2X3501117","_timestamp":"$timestamp","channel":"T_motTemp_Lft","value":5.0}""",
@@ -80,7 +78,7 @@ class StatefulTruckAlertsSuite extends FunSuite with BeforeAndAfter {
       }
     }
 
-    // 3) create a kafka consumer and ensure alerts were created and sent to kafka alert topic
+    // 2) create a kafka consumer and ensure alerts were created and sent to kafka alert topic
     val props = new Properties
     props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer.getKafkaConnect)
     props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
@@ -92,20 +90,20 @@ class StatefulTruckAlertsSuite extends FunSuite with BeforeAndAfter {
     // Subscribe to the topic.
     kc.subscribe(Collections.singletonList("alerts"))
 
-    {
-      var totalCount = 0
-      // keep polling until alerts are generated
-      await().atMost(2, TimeUnit.MINUTES).until(new Callable[lang.Boolean] {
-        override def call(): lang.Boolean = {
-          val consumerRecord = kc.poll(1000)
-          // for now just test count, 2 alerts should have been generated, need to test all use cases and variations
-          // on data
-          totalCount = totalCount + consumerRecord.count
-          if (totalCount == 2) return true
-          false // 2 alerts should have been created based on the json sent to the kafka topic
-        }
-      })
-    }
+    // 3) poll until alert json is in the output sink topic
+    var totalCount = 0
+    // keep polling until alerts are generated
+    await().atMost(2, TimeUnit.MINUTES).until(new Callable[lang.Boolean] {
+      override def call(): lang.Boolean = {
+        val consumerRecord = kc.poll(1000)
+        // for now just test count, 2 alerts should have been generated, need to test all use cases and variations
+        // on data
+        totalCount = totalCount + consumerRecord.count
+        if (totalCount == 2) return true
+        false // 2 alerts should have been created based on the json sent to the kafka topic
+      }
+    })
+
   }
 
   after {
@@ -115,7 +113,10 @@ class StatefulTruckAlertsSuite extends FunSuite with BeforeAndAfter {
       case _: Throwable => // Catching all exceptions and not doing anything with them
     }
     try {
-      es.shutdownNow()
+      // catch exceptions from the driver
+      task.onComplete {
+        case Failure(e) => logger.error(e.toString)
+      }
     } catch {
       case _: Throwable => // Catching all exceptions and not doing anything with them
     }
